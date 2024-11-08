55c73e2690788bb280e8b4d6b0d78fee3265e534
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 15:16:40 -0600

    fixed version

---
+++ Sources/ExampleLibrary/welcomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  welcomeViewController.swift
+//  ExampleLibrary
+//
+//  Created by Oscar Aguilar on 08/11/24.
+//
+
+import UIKit
+
+public class welcomeViewController: UIViewController {
+
+    public static let storyboardVC = UIStoryboard(name: "Storyboard", bundle: Bundle.module).instantiateInitialViewController()!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
