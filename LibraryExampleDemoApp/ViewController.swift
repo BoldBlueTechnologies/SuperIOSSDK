1d16b885de8f0a037e55baba451dcc2a6f63f219
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 10:11:27 -0600

    fix

--- LibraryExampleDemoApp/ViewController.swift
+++ LibraryExampleDemoApp/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-
-        let button = UIButton(type: .system)
-        button.setTitle("Open Insurance Flow", for: .normal)
-        button.addTarget(self, action: #selector(openInsuranceFlow), for: .touchUpInside)
-        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
-        view.addSubview(button)
+//        view.backgroundColor = .white
+//
+//        let button = UIButton(type: .system)
+//        button.setTitle("Open Insurance Flow", for: .normal)
+//        button.addTarget(self, action: #selector(openInsuranceFlow), for: .touchUpInside)
+//        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+//        view.addSubview(button)
     }
 
     @objc private func openInsuranceFlow() {
