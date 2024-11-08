fd000332e031a56a8b26c374318d1e3d7ec3292c
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 15:11:36 -0600

    fix other example

--- Sources/SuperSDK/Views/welcomeViewController.swift
+++ Sources/SuperSDK/Views/welcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  exampleViewController.swift
+//  welcomeViewController.swift
 //  SuperSDK
 //
 //  Created by Oscar Aguilar on 08/11/24.
@@ -7,14 +7,14 @@
 
 import UIKit
 
-public class exampleViewController: UIViewController {
+public class welcomeViewController: UIViewController {
+
     public static let storyboardVC = UIStoryboard(name: "Storyboard", bundle: Bundle.module).instantiateInitialViewController()!
     
-    public override func viewDidLoad() {
+     public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
 }
