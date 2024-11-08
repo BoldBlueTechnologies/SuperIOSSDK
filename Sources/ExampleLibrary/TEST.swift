e383cfc09b992d12566af436e10df3f7408cd778
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 11:39:24 -0600

    fixed ....

---
+++ Sources/ExampleLibrary/TEST.swift
@@ -0,0 +1,18 @@
+//
+//  TEST.swift
+//  ExampleLibrary
+//
+//  Created by Oscar Aguilar on 08/11/24.
+//
+
+import SwiftUI
+
+struct TEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TEST()
+}
