62857b82039ea46f7bfe470f11467e25939c3303
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 15:14:23 -0600

    FIXED TEST 4

--- Package.swift
+++ Package.swift
@@ -1,24 +1,23 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:6.0
+import PackageDescription
 
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "SuperSDK",
+    name: "ExampleLibrary",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SuperSDK",
-            targets: ["SuperSDK"]),
+            name: "ExampleLibrary",
+            targets: ["ExampleLibrary"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SuperSDK"),
-        .testTarget(
-            name: "SuperSDKTests",
-            dependencies: ["SuperSDK"]
-        ),
+            name: "ExampleLibrary",
+            dependencies: []),  // Archivos fuente para el target principal
+        // Archivos de prueba para el target de pruebas
     ]
 )
