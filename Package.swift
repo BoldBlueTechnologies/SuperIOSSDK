f3f7e215f3144e76d23ab461c6a5455330c687c4
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 14:38:50 -0600

    fixed other realese

--- Package.swift
+++ Package.swift
@@ -5,21 +5,19 @@ import PackageDescription
 import PackageDescription
 
 let package = Package(
-    name: "SuperSDK",
+    name: "ExampleLibrary",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SuperSDK",
-            targets: ["SuperSDK"]),
+            name: "ExampleLibrary",
+            targets: ["ExampleLibrary"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SuperSDK"),
-        .testTarget(
-            name: "SuperSDKTests",
-            dependencies: ["SuperSDK"]
-        ),
+            name: "ExampleLibrary",
+            dependencies: []),  // Archivos fuente para el target principal
+        // Archivos de prueba para el target de pruebas
     ]
 )
