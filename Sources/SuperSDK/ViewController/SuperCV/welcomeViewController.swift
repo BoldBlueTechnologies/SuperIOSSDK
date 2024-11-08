8f80c21bbf236d507591c118d45be2801b22891c
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 12:53:59 -0600

    fixed ...

---
+++ Sources/SuperSDK/ViewController/SuperCV/welcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  welcomeViewController.swift
+//  SuperSDK
+//
+//  Created by Oscar Aguilar on 08/11/24.
+//
+
+import UIKit
+
+class welcomeViewController: UIViewController {
+
+    public static let storyboardVC = UIStoryboard(name: "Storyboard", bundle: Bundle.module).instantiateInitialViewController()!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
