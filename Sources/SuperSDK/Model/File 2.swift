e578e729d4ada94932c697a203a58084b3891f90
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 16:35:07 -0600

    fixed ....

--- Sources/SuperSDK/Model/File 2.swift
+++ Sources/SuperSDK/Model/File 2.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 struct Pokemon: Decodable {
+    let count: Int
+    let next: String
+    let previous: String?
+    let results: [PokemonInfo]
+}
+
+struct PokemonInfo: Codable {
     let name: String
     let url: String
 }
