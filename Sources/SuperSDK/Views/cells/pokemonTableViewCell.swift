c1bae2c11ea071910e089e0f8e7cef2c9138352e
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 16:08:46 -0600

    fixed ....

--- Sources/SuperSDK/Views/cells/pokemonTableViewCell.swift
+++ Sources/SuperSDK/Views/cells/pokemonTableViewCell.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-class pokemonTableViewCell: UITableViewCell {
+public class pokemonTableViewCell: UITableViewCell {
 
     @IBOutlet weak var pokemonimage: UIImageView!
     @IBOutlet weak var nombreLabel: UILabel!
     
-    override func awakeFromNib() {
+    public override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+   public override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
