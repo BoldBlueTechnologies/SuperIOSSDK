a3a48beaa74a106555968554be5057db0a4433a6
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-07 18:04:59 -0600

    fix prueba storyboard

--- Sources/ExampleLibrary/FirstViewController.swift
+++ Sources/ExampleLibrary/FirstViewController.swift
@@ -11,17 +11,17 @@ public class FirstViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
-
-        let button = UIButton(type: .system)
-        button.setTitle("Go to Second Screen v2", for: .normal)
-        button.addTarget(self, action: #selector(goToSecondScreen), for: .touchUpInside)
-        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
-        view.addSubview(button)
+//        view.backgroundColor = .purple
+//
+//        let button = UIButton(type: .system)
+//        button.setTitle("Go to Second Screen v2", for: .normal)
+//        button.addTarget(self, action: #selector(goToSecondScreen), for: .touchUpInside)
+//        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+//        view.addSubview(button)
     }
 
-    @objc private func goToSecondScreen() {
-        let secondVC = SecondViewController()
-        navigationController?.pushViewController(secondVC, animated: true)
-    }
+//    @objc private func goToSecondScreen() {
+//        let secondVC = SecondViewController()
+//        navigationController?.pushViewController(secondVC, animated: true)
+//    }
 }
