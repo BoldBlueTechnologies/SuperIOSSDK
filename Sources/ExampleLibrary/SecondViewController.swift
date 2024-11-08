fdc17045ad49ea885abb8cdf66948ccbcf67cf59
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 11:58:09 -0600

    fixed ....

--- Sources/ExampleLibrary/SecondViewController.swift
+++ Sources/ExampleLibrary/SecondViewController.swift
@@ -11,13 +11,23 @@ public class SecondViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .purple
 
-        let label = UILabel()
-        label.text = "This is the second screen!"
-        label.textAlignment = .center
-        label.frame = CGRect(x: 50, y: 200, width: 300, height: 50)
-        view.addSubview(label)
+        let button = UIButton(type: .system)
+        button.setTitle("Start", for: .normal)
+        button.addTarget(self, action: #selector(openInsuranceFlow), for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        view.addSubview(button)
+    }
+    
+    @objc private func openInsuranceFlow() {
+        
+        let storyboard = UIStoryboard(name: "SuperSDK", bundle: nil)
+        let switchViewController = storyboard.instantiateViewController(
+            withIdentifier: "FirstView") as! FirstViewController
+        switchViewController.modalPresentationStyle = .popover
+        switchViewController.isModalInPresentation = true
+        self.present(UINavigationController(rootViewController: switchViewController), animated: true, completion: nil)
     }
 }
 
