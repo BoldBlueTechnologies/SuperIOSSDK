e41b812ca64c80196622d084f2f86bba793a4823
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 14:44:46 -0600

    fixed other realeaase v2

---
+++ Sources/ExampleLibrary/exampleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  exampleViewController.swift
+//  ExampleLibrary
+//
+//  Created by Oscar Aguilar on 08/11/24.
+//
+
+import UIKit
+
+class exampleViewController: UIViewController {
+
+    public static let storyboardVC = UIStoryboard(name: "exampleViewController", bundle: Bundle.module).instantiateInitialViewController()!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
