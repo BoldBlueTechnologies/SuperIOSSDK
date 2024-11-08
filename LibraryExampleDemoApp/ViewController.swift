24d21ebd048bc32aad4e2ceec0d76a84f2fe6870
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-08 11:56:16 -0600

    fixed ... other

--- LibraryExampleDemoApp/ViewController.swift
+++ LibraryExampleDemoApp/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 //        view.backgroundColor = .white
 //
-//        let button = UIButton(type: .system)
-//        button.setTitle("Open Insurance Flow", for: .normal)
-//        button.addTarget(self, action: #selector(openInsuranceFlow), for: .touchUpInside)
-//        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
-//        view.addSubview(button)
+        let button = UIButton(type: .system)
+        button.setTitle("Start", for: .normal)
+        button.addTarget(self, action: #selector(openInsuranceFlow), for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        view.addSubview(button)
     }
 
     @objc private func openInsuranceFlow() {
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
 //        present(navigationController, animated: true, completion: nil)
         
         
-        let storyboard = UIStoryboard(name: "SuperMx", bundle: nil)
+        let storyboard = UIStoryboard(name: "SuperSDK", bundle: nil)
         let switchViewController = storyboard.instantiateViewController(
             withIdentifier: "FirstView") as! FirstViewController
         switchViewController.modalPresentationStyle = .popover
--- Sources/ExampleLibrary/FirstViewController.swift
+++ Sources/ExampleLibrary/FirstViewController.swift
@@ -9,29 +9,29 @@ import UIKit
 
 public class FirstViewController: UIViewController {
     
-    lazy private var sampleLabel: UILabel = {
-           let label = UILabel()
-           label.translatesAutoresizingMaskIntoConstraints = false
-           label.text = "Hello World!"
-           label.textColor = UIColor.white
-           return label
-       }()
+//    lazy private var sampleLabel: UILabel = {
+//           let label = UILabel()
+//           label.translatesAutoresizingMaskIntoConstraints = false
+//           label.text = "Hello World!"
+//           label.textColor = UIColor.white
+//           return label
+//       }()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.black
-        self.view.addSubview(self.sampleLabel)
-        self.setUpConstraints()
+//        self.view.backgroundColor = UIColor.black
+//        self.view.addSubview(self.sampleLabel)
+//        self.setUpConstraints()
     }
     
-    func setUpConstraints() {
-        let sampleLabelConstraints = [
-            self.sampleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.sampleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        ]
-        NSLayoutConstraint.activate(sampleLabelConstraints)
-    }
+//    func setUpConstraints() {
+//        let sampleLabelConstraints = [
+//            self.sampleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+//            self.sampleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+//        ]
+//        NSLayoutConstraint.activate(sampleLabelConstraints)
+//    }
     
 }
 
