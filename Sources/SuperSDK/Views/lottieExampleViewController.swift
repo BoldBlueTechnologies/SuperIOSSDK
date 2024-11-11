c27e8dfd39e0b305cdc9e3675dc1045a23485889
oaguilar016 <84034393+oaguilar016@users.noreply.github.com>
2024-11-11 17:54:41 -0600

    fixed ....

--- Sources/SuperSDK/Views/lottieExampleViewController.swift
+++ Sources/SuperSDK/Views/lottieExampleViewController.swift
@@ -10,33 +10,38 @@ import Lottie
 
 class lottieExampleViewController: UIViewController {
 
-    var animationView: LottieAnimationView?
+    private var animationView: LottieAnimationView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        animationView = LottieAnimationView(name: "frankie")
-//        animationView = .init(name: "frankie")
-          
-          animationView!.frame = view.bounds
-          
-          // 3. Set animation content mode
-          
-          animationView!.contentMode = .scaleAspectFit
-          
-          // 4. Set animation loop mode
-          
-          animationView!.loopMode = .loop
-          
-          // 5. Adjust animation speed
-          
-          animationView!.animationSpeed = 0.5
-          
-          view.addSubview(animationView!)
-          
-          // 6. Play animation
-          
-          animationView!.play()
+        guard let url = URL(string: "https://assets5.lottiefiles.com/datafiles/zc3XRzudyWE36ZBJr7PIkkqq0PFIrIBgp4ojqShI/newAnimation.json") else {
+            return
+        }
+        
+        // Initialize the AnimationView with the animation name
+        animationView = LottieAnimationView()
+        
+        // Ensure the animation was loaded successfully
+        guard let animationView = animationView else { return }
+        
+        // Set the frame or use Auto Layout
+        animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationView.center = view.center
+        
+        // Configure animation properties
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 1.0
+        
+        // Add the animation view to the main view
+        view.addSubview(animationView)
+         
+        LottieAnimation.loadedFrom(url: url, closure: { [weak self] animation in
+            self?.animationView?.animation = animation
+            self?.animationView?.loopMode = .loop
+            self?.animationView?.play()
+        }, animationCache: DefaultAnimationCache.sharedCache)
         
     }
     
